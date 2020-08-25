b8044d81f7e3c129457d37a8758f8983bd278568
ronkyzhong <1014325621@qq.com>
2020-08-26 00:28:03 +0800

    Calculator testcase

--- test_calculator/test_calculator.py
+++ test_calculator/test_calculator.py
@@ -1,52 +1,37 @@
-import pytest
+import json
 
+import pytest
+import yaml
+import json
 from calculator.calculator import Calculator
+
+with open("../data/test_calculator_data.yaml", 'r') as f:
+    data = yaml.safe_load(f)
 
 
 class TestCalculator:
-    @pytest.mark.parametrize('a,b,result', [(1, 1, 2),
-                                            (0.1, 0.1, 0.2),
-                                            (2, 0.1, 2.1),
-                                            (-1, 2, 1),
-                                            (-1, -2, -3),
-                                            ],
+    @pytest.mark.parametrize('a,b,result', data['add'],
                              ids=("int", "float", "int_and_float", "negative_and_int", "negative"))
     def test_add(self, a, b, result, calc):
         cal = Calculator(a, b)
         assert result == cal.add()
 
-    @pytest.mark.parametrize('a,b,result', [(2, 1, 1),
-                                            (0.2, 0.1, 0.1),
-                                            (2.2, 0.1, 2.1),
-                                            (-1, 2, -3),
-                                            (-1, -2, 1),
-                                            ],
+    @pytest.mark.parametrize('a,b,result', data['sub'],
                              ids=("int", "float", "int_and_float", "negative_and_int", "negative"))
     def test_sub(self, a, b, result, calc):
         cal = Calculator(a, b)
         assert result == cal.sub()
 
-    @pytest.mark.parametrize('a,b,result', [(2, 1, 2),
-                                            (0.2, 0.1, 0.02),
-                                            (2.2, 0.1, 0.22),
-                                            (-1, 2, -2),
-                                            (-1, -2, 2),
-                                            ],
+    @pytest.mark.parametrize('a,b,result', data['mul'],
                              ids=("int", "float", "int_and_float", "negative_and_int", "negative"))
     def test_mul(self, a, b, result, calc):
         cal = Calculator(a, b)
         assert result == cal.mul()
 
-    @pytest.mark.parametrize('a,b,result', [(2, 1, 2),
-                                            (0.2, 0.1, 2),
-                                            (2.2, 0.1, 22),
-                                            (-1, 2, -0.5),
-                                            (-1, -2, 0.5),
-                                            (-1, 0, 0),
-                                            (-1, 0.0, 0)
-                                            ],
+    @pytest.mark.parametrize('a,b,result', data['div'],
                              ids=(
-                             "int", "float", "int_and_float", "negative_and_int", "negative", "div is 0", "div is 0.0"))
+                                     "int", "float", "int_and_float", "negative_and_int", "negative", "div is 0",
+                                     "div is 0.0"))
     def test_div(self, a, b, result, calc):
         cal = Calculator(a, b)
         assert result == cal.div()
